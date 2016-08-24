ec9aa8ebc82059fad3a90dac70efd36cde33b063
JNfeicui-caiwei <wonner_cw@163.com>
2016-08-24 16:48:25 +0800

    Initial commit

---
+++ app/src/main/java/com/sjsm/swipemenulistviewdemo/MyAdapter.java
@@ -0,0 +1,55 @@
+package com.sjsm.swipemenulistviewdemo;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class MyAdapter extends BaseAdapter {
+    private Context context;
+    private List<String> list;
+
+    public MyAdapter(Context context, List<String> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            convertView = View.inflate(context, R.layout.list_item, null);
+            holder = new ViewHolder();
+            holder.txt = (TextView) convertView.findViewById(R.id.tv_text);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.txt.setText(list.get(position));
+        return convertView;
+    }
+
+    public class ViewHolder {
+        private TextView txt;
+    }
+
+}
